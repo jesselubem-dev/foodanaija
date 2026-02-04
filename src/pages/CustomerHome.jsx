1fba0abbaa57822e6a9e4cb093d0f14a828ae802
jesse <jesselubem@gmail.com>
2026-02-04 14:02:03 +0000

    File changes

--- src/pages/CustomerHome.jsx
+++ src/pages/CustomerHome.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { 
   Search, MapPin, Star, Clock, Bike, ChefHat, ShoppingBag, History, LogOut, ChevronRight, MessageSquare, Headset
 } from 'lucide-react';
