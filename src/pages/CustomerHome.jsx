6cb2f0ca95a87c092404e17b8039471d2f09d305
jesse <jesselubem@gmail.com>
2026-02-07 00:43:45 +0000

    File changes

--- src/pages/CustomerHome.jsx
+++ src/pages/CustomerHome.jsx
@@ -383,7 +383,7 @@ function CustomerHomeContent() {
         {user && (
           <div className="pt-6 pb-4">
             <h2 className="text-2xl font-bold text-gray-900">
-              {t('hi')}, {user.full_name.split(' ')[0]}! 👋
+              {t('hi')}, {user.full_name.split(' ')[0]}! <span className="inline-block animate-wave">👋</span>
             </h2>
             <p className="text-sm text-gray-500 mt-1">{t('whatToEat')}</p>
           </div>
