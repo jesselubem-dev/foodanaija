702485cd00d605856f442186c58179a2bc5da1bf
jesse <jesselubem@gmail.com>
2026-03-12 09:34:06 +0000

    File changes

--- src/pages/CustomerHome.jsx
+++ src/pages/CustomerHome.jsx
@@ -586,6 +586,9 @@ function CustomerHomeContent() {
         )}
       </div>
 
+        {/* Ramadan Banner */}
+        <RamadanBanner />
+
         {/* Promo Modal */}
         {showPromo && promoItems.length > 0 && (
           <PromoModal 
