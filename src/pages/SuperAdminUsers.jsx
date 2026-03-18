68218ae1a9230f295dadeb4c8711e9aaae41ec61
jesse <jesselubem@gmail.com>
2026-03-18 08:15:10 +0000

    File changes

--- src/pages/SuperAdminUsers.jsx
+++ src/pages/SuperAdminUsers.jsx
@@ -4,7 +4,7 @@ import { createPageUrl } from '../utils';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { 
-  Users, User, Mail, Calendar, ArrowLeft, Search, Shield, UserPlus
+  Users, User, Mail, Calendar, ArrowLeft, Search, Shield, UserPlus, Download
 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
